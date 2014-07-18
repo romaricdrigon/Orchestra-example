4b5d4a8e0aa25a23309f5ce8a7601bf204838413
Romaric Drigon <romaric.drigon@gmail.com>
2014-07-18 14:36:13 +0200

    Using Orchestra bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new RomaricDrigon\OrchestraBundle\RomaricDrigonOrchestraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
