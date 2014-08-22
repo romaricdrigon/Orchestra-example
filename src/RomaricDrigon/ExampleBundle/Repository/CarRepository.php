d3637d406082c3473041f0450082f25bbe2ba23d
Romaric Drigon <romaric.drigon@gmail.com>
2014-08-22 16:20:40 +0200

    Added CarRepository example

---
+++ src/RomaricDrigon/ExampleBundle/Repository/CarRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\ExampleBundle\Repository;
+use RomaricDrigon\OrchestraBundle\Domain\RepositoryInterface;
+
+/**
+ * Class CarRepository
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ */
+class CarRepository implements RepositoryInterface
+{
+} 
